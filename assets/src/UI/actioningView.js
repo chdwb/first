96bce901b0a395f63b0372e2e4ff196ed0c78017
wubo <158205186@qq.com>
2017-07-28 23:41:45 +0800

    add action

---
+++ assets/src/UI/actioningView.js
@@ -0,0 +1,76 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        actionBg: {
+            default: null,
+            type: cc.Sprite
+        },
+        process: {
+            default: null,
+            type: cc.ProgressBar
+        },
+        processText: {
+            default: null,
+            type: cc.Label
+        },
+        DescText: {
+            default: null,
+            type: cc.Label
+        },
+        rightNowBtn: {
+            default: null,
+            type: cc.Node
+        },
+        backBtn: {
+            default: null,
+            type: cc.Node
+        },
+        totalTime: 0,
+        currentTime: 0,
+        isAction: false,
+        handle: null
+    },
+
+    setActionInfo: function(time, desc, image, callback) {
+        this.isAction = true
+        this.totalTime = time
+        this.currentTime = 0
+        this.DescText.string = desc
+        this.handle = callback
+    },
+    // use this for initialization
+    onLoad: function() {
+        this.backBtn.on("click", (event) => {
+
+        }, this.backBtn)
+
+        this.rightNowBtn.on("click", (event) => {
+
+        }, this.rightNowBtn)
+
+        this.setActionInfo(10, "test", "", this.cccc)
+    },
+
+    update: function(dt) {
+        if (this.isAction) {
+            this.currentTime += dt;
+            if (this.currentTime >= this.totalTime) {
+                this.process.progress = 1.0
+                this.processText.string = 100 + "%"
+                this.handle()
+                this.isAction = false;
+                this.currentTime = 0
+                this.totalTime = 0
+            } else {
+                this.process.progress = this.currentTime / this.totalTime
+                this.processText.string = parseInt(this.process.progress * 100.0) + "%"
+            }
+        }
+
+    },
+    // called every frame, uncomment this function to activate update callback
+    // update: function (dt) {
+
+    // },
+});
