32019d496eb9908d286cc9195ed281eef3fad143
wubo <158205186@qq.com>
2017-10-11 19:51:58 +0800

    ccc

--- assets/src/UI/VideoCollectView.js
+++ assets/src/UI/VideoCollectView.js
@@ -23,171 +23,156 @@ cc.Class({
         },
 
         scrollView: {
-        	default: null,
-        	type: cc.ScrollView
+            default: null,
+            type: cc.ScrollView
         },
-        BgNode:{
+        BgNode: {
             type: cc.Node,
             default: null
         },
-        VideoNode:{
+        VideoNode: {
             type: cc.Node,
             default: null
         },
         spawnCount: 0, // 总个数
         totalRow: 0, // 行数
         spacing: 60, // space between each item
-        
+
     },
 
     // use this for initialization
 
     setGold: function(gold) {
-        cc.log("set gold"+gold)
+        cc.log("set gold" + gold)
         this.goldText.string = gold + ""
     },
-    updateui:function(){
+    updateui: function() {
 
         this.setGold(cc.cs.PlayerInfo.money)
-         this.initialize(cc.cs.gameData.video);
+        this.initialize(cc.cs.gameData.video);
     },
 
-    onBack:function()
-    {
+    onBack: function() {
         cc.cs.UIMgr.closeView()
     },
-    onLoad: function () {
+    onLoad: function() {
 
         this.content = this.scrollView.content;
         this.prefab = cc.loader.getRes("prefab/VideoItem", cc.Prefab)
-        
-       
+
+
 
     },
-    goshop:function(){
+    goshop: function() {
         cc.log("goShop LoveView")
         cc.cs.UIMgr.setShopType(2)
         cc.log("cc.cs.UIMgr = " + cc.cs.UIMgr.currentShopType)
         cc.cs.UIMgr.openView(cc.cs.UIMgr.SHOPVIEW)
-    }
-    ,
+    },
 
-    onEnable:function()
-    {
+    onEnable: function() {
         this.updateui()
 
         var isshowpop = true
-         for (var i = 0; i < cc.cs.PlayerInfo.playerhotpacks.length; i++) 
-                {
-                cc.log("update HOT item "+ cc.cs.PlayerInfo.playerhotpacks[i].video_id)
-                if (cc.cs.PlayerInfo.playerhotpacks[i].hot_id == 7)
-                 {
-                    isshowpop = false
-                    break;
-                 }
-                }
-                if(isshowpop)
-                {
-                    cc.cs.UIMgr.showPopBuy(7, this.buyPop, this)
-                }
-    }
-    ,
+        for (var i = 0; i < cc.cs.PlayerInfo.playerhotpacks.length; i++) {
+            cc.log("update HOT item " + cc.cs.PlayerInfo.playerhotpacks[i].video_id)
+            if (cc.cs.PlayerInfo.playerhotpacks[i].hot_id == 7) {
+                isshowpop = false
+                break;
+            }
+        }
+        if (isshowpop) {
+            cc.cs.UIMgr.showPopBuy(7, this.buyPop, this)
+        }
+    },
 
-    buyPop:function()
-    {
+    buyPop: function() {
 
-        cc.cs.gameMgr.sendGoodBuy( 3,7, 1, this.onLibaohandle, this)
+        cc.cs.gameMgr.sendGoodBuy(3, 7, 1, this.onLibaohandle, this)
 
     },
 
-      onLibaohandle:function(ret)
-    {
+    onLibaohandle: function(ret) {
         cc.log("libao update")
         var JasonObject = JSON.parse(ret);
-		cc.cs.UIMgr.closeNetView()
+        cc.cs.UIMgr.closeNetView()
         if (JasonObject.success === true) {
             //cc.cs.UIMgr.closeNetView()
             cc.cs.UIMgr.showTip("购买成功", 1.0)
             var parent = this.node.parent
-            
+
             cc.cs.PlayerInfo.money = JasonObject.content.info.money
             cc.cs.PlayerInfo.refreshInfoData(JasonObject.content.info)
             this.updateui()
-           
-            
-            
+
+
+
         } else {
             cc.cs.UIMgr.showTip(JasonObject.error, 1.0)
         }
     },
 
-    
 
-    initialize: function (itemArray) {
+
+    initialize: function(itemArray) {
         cc.log("alkdjflkajdflkajf")
-        
+
         //this.Exp.string = ""+cc.cs.PlayerInfo.exp
         this.content.removeAllChildren(true);
         this.prefab = cc.loader.getRes("prefab/VideoItem", cc.Prefab)
         var length = itemArray["TOTAL_COUNT"]
-        cc.log( "arraylength"+length)
-        if(length % 4 == 0)
-        {
+        cc.log("arraylength" + length)
+        if (length % 4 == 0) {
             this.totalCount = Math.floor(length / 4);
-        }
-        else
-        {
+        } else {
             this.totalCount = Math.floor(length / 4) + 1;
         }
 
         this.spawnCount = length;
-        cc.log("total count "+this.totalCount)
+        cc.log("total count " + this.totalCount)
         this.content.height = this.totalCount * (this.prefab.data.height + this.spacing)
         cc.log("prefab height " + this.prefab.data.height)
         var littlewidth = this.scrollView.content.width / 4
         var halfwidth = this.scrollView.content.width / 2
         var count = 0;
-        for(var i in itemArray) { // spawn items, we only need to do this once
-    		cc.log("i = ",i);
-    		if(i != "TOTAL_COUNT" && i != "LAST" && i != "FIRST" )
-            {
+        for (var i in itemArray) { // spawn items, we only need to do this once
+            cc.log("i = ", i);
+            if (i != "TOTAL_COUNT" && i != "LAST" && i != "FIRST") {
                 var item = cc.instantiate(this.prefab)
 
 
 
-                var offset =  (this.scrollView.content.width - item.width*4)/8
-                
+                var offset = (this.scrollView.content.width - item.width * 4) / 8
+
                 cc.log("item height " + item.height)
                 var itemCom = item.getComponent("VideoItem")
+
                 var videodata = cc.cs.gameData.video[i]
-               //cc.log("gooddata = "+gooddata)
-                if(videodata != undefined)
-                {
+                    //cc.log("gooddata = "+gooddata)
+                if (videodata != undefined) {
                     //itemCom.setItmeNmae(gooddata["GOODS_NAME"] )
                     //cc.log("goodsname = "+videodata["GOODS_NAME"])
                     itemCom.setVideo(videodata, this.BgNode, this.VideoNode)
+                    itemCom.videoView = this
                 }
                 this.content.addChild(item);
-        		//item.setPosition(0, -item.height * (0.5 + i) - this.spacing * (i + 1));
+                //item.setPosition(0, -item.height * (0.5 + i) - this.spacing * (i + 1));
                 let PosY = 0;
-                if((count+1) % 4 == 0)
-                {
-                    PosY =  Math.floor((count+1) / 4);
+                if ((count + 1) % 4 == 0) {
+                    PosY = Math.floor((count + 1) / 4);
+                } else {
+                    PosY = Math.floor((count + 1) / 4) + 1;
                 }
-                else
-                {
-                    PosY =Math.floor((count+1) /4) +1;
-                }   
-            
+
                 // this.scrollView.content.width / 4
-                PosY -- ;
-                 
-                 var pos = count % 4
-            
-               item.setPosition(- halfwidth + littlewidth*pos +item.width/2 + offset,-item.height/2 - PosY * (item.height + this.spacing))
-               count ++;
+                PosY--;
+
+                var pos = count % 4
+
+                item.setPosition(-halfwidth + littlewidth * pos + item.width / 2 + offset, -item.height / 2 - PosY * (item.height + this.spacing))
+                count++;
             }
-    	}
+        }
     },
 
     // called every frame, uncomment this function to activate update callback
--- assets/src/UI/prefb/VideoItem.js
+++ assets/src/UI/prefb/VideoItem.js
@@ -12,149 +12,143 @@ cc.Class({
         //    readonly: false,    // optional, default is false
         // },
         // ...
-        lock:{
-            default:null,
-            type : cc.Node
+        lock: {
+            default: null,
+            type: cc.Node
         }
-        
+
         ,
-        
-        Icon:{
-            default:null,
-            type : cc.Sprite
-        },
-        
-        Play:{
-            default:null,
-            type : cc.Node
+
+        Icon: {
+            default: null,
+            type: cc.Sprite
         },
 
-        JinBi:{
+        Play: {
+            default: null,
+            type: cc.Node
+        },
 
-            default:null,
-            type : cc.Node
+        JinBi: {
+
+            default: null,
+            type: cc.Node
 
         },
-        
-        Name:{
-            
-            default:null,
-            type : cc.Label
+
+        Name: {
+
+            default: null,
+            type: cc.Label
         },
-        Price:{
-            
-            default:null,
-            type : cc.Label
+        Price: {
+
+            default: null,
+            type: cc.Label
         },
-        videoData:null,
-        videoID:0,
-        isBuy:false,
-        bgNode:null,
-        videoNode:null,
+        videoData: null,
+        videoID: 0,
+        isBuy: false,
+        bgNode: null,
+        videoNode: null,
+        videoView: null,
     },
 
     // use this for initialization
-    onLoad: function () {
+    onLoad: function() {
         //this.Play.active = false
         //this.JinBi.active = true
-        
+
 
     },
 
-    updateui:function()
-    {
-        
-         for (var i = 0; i < cc.cs.PlayerInfo.playerbuyvideos.length; i++) {
-             cc.log("update video item "+ cc.cs.PlayerInfo.playerbuyvideos[i].video_id)
+    updateui: function() {
+
+        for (var i = 0; i < cc.cs.PlayerInfo.playerbuyvideos.length; i++) {
+            cc.log("update video item " + cc.cs.PlayerInfo.playerbuyvideos[i].video_id)
             if (cc.cs.PlayerInfo.playerbuyvideos[i].video_id == this.videoID) {
-                
+
                 this.isBuy = true
                 break;
             }
         }
 
-        this.Play.active =  this.isBuy
-        this.JinBi.active = ! this.isBuy
+
+        this.Play.active = this.isBuy
+        this.JinBi.active = !this.isBuy
 
     },
 
-      sendbuy:function()
-  {
-     
-      cc.cs.gameMgr.sendGoodBuy(5,this.videoID, 1, this.onbuyhandle, this)
-  },
+    sendbuy: function() {
+
+        cc.cs.gameMgr.sendGoodBuy(5, this.videoID, 1, this.onbuyhandle, this)
+    },
 
 
-   onbuyhandle:function(ret)
-    {
-       
+    onbuyhandle: function(ret) {
+
         var JasonObject = JSON.parse(ret);
-		cc.cs.UIMgr.closeNetView()
+        cc.cs.UIMgr.closeNetView()
         if (JasonObject.success === true) {
             //cc.cs.UIMgr.closeNetView()
             cc.cs.UIMgr.showTip("购买成功", 1.0)
             var parent = this.node.parent
-            
+
             cc.cs.PlayerInfo.money = JasonObject.content.info.money
             cc.cs.PlayerInfo.refreshInfoData(JasonObject.content.info)
             this.updateui()
-            
+            if (this.videoView != null) {
+                this.videoView.setGold(cc.cs.PlayerInfo.money)
+            }
+
         } else {
             cc.cs.UIMgr.showTip(JasonObject.error, 1.0)
         }
     },
 
-      goShop:function()
-    {
-       
+    goShop: function() {
+
         cc.cs.UIMgr.setShopType(2)
         cc.log("cc.cs.UIMgr = " + cc.cs.UIMgr.currentShopType)
         cc.cs.UIMgr.openView(cc.cs.UIMgr.SHOPVIEW)
 
     },
 
-    onClick:function()
-    {
+    onClick: function() {
 
-        if(this.isBuy == false)
-        {
-        
-            var needgold =   this.videoData["VIDEO_NEED_MONEY"]
+        if (this.isBuy == false) {
+
+            var needgold = this.videoData["VIDEO_NEED_MONEY"]
             var job = this.videoData["VIDEO_NAME"]
 
-            if( parseInt(needgold) > cc.cs.PlayerInfo.money)
-            {
-                cc.cs.UIMgr.showPopupOC("提示","没有足够的金币，是否前往购买",this.goShop,null)
+            if (parseInt(needgold) > cc.cs.PlayerInfo.money) {
+                cc.cs.UIMgr.showPopupOC("提示", "没有足够的金币，是否前往购买", this.goShop, null)
+            } else {
+                cc.cs.UIMgr.showPopupOC("提示", "购买视频" + job + "需要花费" + needgold + "金币，确定购买吗？", this.sendbuy.bind(this), null)
             }
-            else
-            {
-                cc.cs.UIMgr.showPopupOC("提示","购买视频"+job+"需要花费"+needgold+"金币，确定购买吗？",this.sendbuy.bind(this),null)
-            }
-        }
-        else
-        {
-             //cc.cs.UIMgr.openView(cc.cs.UIMgr.VIDEOVIEW)
-             this.bgNode.active = false
-             this.videoNode.getComponent("jsVideo").setPlayVideoID(this.videoID)
-             this.videoNode.active = true
+        } else {
+            //cc.cs.UIMgr.openView(cc.cs.UIMgr.VIDEOVIEW)
+            this.bgNode.active = false
+            this.videoNode.getComponent("jsVideo").setPlayVideoID(this.videoID)
+            this.videoNode.active = true
         }
 
     },
 
-    setVideo: function( videodata ,bgNode, videNode){
+    setVideo: function(videodata, bgNode, videNode) {
         this.videoData = videodata
         this.videoID = videodata["VIDEO_ID"];
         this.Name.string = videodata["VIDEO_NAME"]
         this.Price.string = videodata["VIDEO_NEED_MONEY"]
-        //this.itemDec.string = good["GOODS_DESC"]
+            //this.itemDec.string = good["GOODS_DESC"]
         cc.log(this.videoID)
         cc.cs.UIMgr.changeSprite(this.Icon.node, "video_icon/" + this.videoID)
         this.updateui()
         this.bgNode = bgNode
         this.videoNode = videNode
     }
-    
-    
+
+
 
     // called every frame, uncomment this function to activate update callback
     // update: function (dt) {
