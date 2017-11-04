a55fe56bf2a66aedd2740d50e148dd1cd58cc9dd
cy <yun_cao@live.com>
2017-11-04 16:45:06 +0800

    c

--- assets/src/UI/login.js
+++ assets/src/UI/login.js
@@ -1123,9 +1123,9 @@ cc.Class({
 
             })
         } else {
-            cc.loader.loadResDir("picture/newRes831", (current, total, item) => {
+            cc.loader.loadResDir("picture/newRes831/moment", (current, total, item) => {
                 if (total == 0) return
-                self.loadProcessPer = parseInt((parseFloat(current) / parseFloat(total)) * 90.0)
+                self.loadProcessPer = parseInt((parseFloat(current) / parseFloat(total)) * 80.0)
                 cc.log("(this.loadPress  + this.loadPress)  = " + (self.loadProcessPer + self.loadScenePer))
                 self.perLaber.string = (self.loadProcessPer + self.loadScenePer) + "%"
                 if (self.loadProcessPer + self.loadScenePer >= 100) {
@@ -1134,6 +1134,27 @@ cc.Class({
 
             }, (err, ass) => {
 
+            })
+			
+			cc.loader.loadResDir("picture/newRes831/love_job", (current, total, item) => {
+                
+
+            }, (err, ass) => {
+					self.loadProcessPer += 5 
+					self.perLaber.string = (self.loadProcessPer + self.loadScenePer) + "%"
+					if (self.loadProcessPer + self.loadScenePer >= 100) {
+						cc.director.loadScene('GameScene');
+					}
+            })
+			
+			cc.loader.loadResDir("picture/newRes831/shop", (current, total, item) => {
+
+            }, (err, ass) => {
+					self.loadProcessPer += 5 
+					self.perLaber.string = (self.loadProcessPer + self.loadScenePer) + "%"
+					if (self.loadProcessPer + self.loadScenePer >= 100) {
+						cc.director.loadScene('GameScene');
+					}
             })
         }
 
